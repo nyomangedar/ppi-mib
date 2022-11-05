cd823a2384ab913eebf217e501517bc78418ef36
IXM138 <nyoman.gedar@gmail.com>
2022-11-05 12:34:58 +0000

    fixing api endpoint

--- frontend-app/src/app/api/apiSlice.js
+++ frontend-app/src/app/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
     baseQuery: fetchBaseQuery({
         // baseUrl: "http://localhost:3500",
-        baseUrl: "http://167.99.198.188/",
+        baseUrl: "http://167.99.198.188:3500/",
         // process.env.NODE_ENV === "development"
         //     ? "http://localhost:3500"
         //     : "http://167.99.198.188/",
