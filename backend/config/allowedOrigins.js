71c5de16c633ac2f68fc2c518a52cb399a062483
IXM138 <nyoman.gedar@gmail.com>
2022-11-05 02:47:08 +0000

    try fixing cors

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins =
-    process.env.NODE_ENV === "development"
-        ? ["http://localhost:3000"]
-        : ["http://167.99.198.188/"];
+    // process.env.NODE_ENV === "development"
+    //     ? ["http://localhost:3000"]
+    //     : ["http://167.99.198.188/"];
+    ["http://167.99.198.188/"];
 
 module.exports = allowedOrigins;
--- frontend-app/src/app/api/apiSlice.js
+++ frontend-app/src/app/api/apiSlice.js
@@ -2,10 +2,10 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
     baseQuery: fetchBaseQuery({
-        baseUrl:
-            process.env.NODE_ENV === "development"
-                ? "http://localhost:3500"
-                : "http://167.99.198.188/",
+        baseUrl: "http://167.99.198.188/",
+        // process.env.NODE_ENV === "development"
+        //     ? "http://localhost:3500"
+        //     : "http://167.99.198.188/",
     }),
     tagTypes: ["Post", "User"],
     endpoints: (builder) => ({}),
