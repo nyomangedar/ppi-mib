f9b03ffdd492ab039b281883448b878d6715836b
adityojulian <axm1882@student.bham.ac.uk>
2022-11-06 21:10:06 +0000

    changed website description and footer year

--- frontend-app/src/components/Footer.jsx
+++ frontend-app/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
 				<footer class="">
 					<div class="mt-5 footer-ppi d-flex flex-column justify-content-center align-items-center">
 						<span class="text-light h6" style={{ fontWeight: 800 }}>
-							Copyright 2002 <span style={{ color: "#1D1D59" }}>PPI MIB</span>.
+							Copyright 2022 <span style={{ color: "#1D1D59" }}>PPI MIB</span>.
 							All rights reserved
 						</span>
 					</div>
