958ba57c2dd055e2895aa52a12f51aa0e887fbc0
adityojulian <axm1882@student.bham.ac.uk>
2022-11-06 03:30:58 +0000

    changed base url to correct ip

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,8 +1,8 @@
 const allowedOrigins =
-    // ["http://localhost:3000"];
-    process.env.NODE_ENV === "development"
-        ? ["http://localhost:3000"]
-        : ["http://167.99.198.188"];
+	// ["http://localhost:3000"];
+	process.env.NODE_ENV === "development"
+		? ["http://localhost:3000"]
+		: ["http://68.183.32.204"];
 // ["http://167.99.198.188/"];
 
 module.exports = allowedOrigins;
--- frontend-app/src/app/api/apiSlice.js
+++ frontend-app/src/app/api/apiSlice.js
@@ -1,14 +1,14 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
-    baseQuery: fetchBaseQuery({
-        // baseUrl: "http://localhost:3500",
-        // baseUrl: "http://167.99.198.188:3500/",
-        baseUrl:
-            process.env.NODE_ENV === "development"
-                ? "http://localhost:3500"
-                : "http://167.99.198.188:3500",
-    }),
-    tagTypes: ["Post", "User"],
-    endpoints: (builder) => ({}),
+	baseQuery: fetchBaseQuery({
+		// baseUrl: "http://localhost:3500",
+		// baseUrl: "http://167.99.198.188:3500/",
+		baseUrl:
+			process.env.NODE_ENV === "development"
+				? "http://localhost:3500"
+				: "http://68.183.32.204:3500",
+	}),
+	tagTypes: ["Post", "User"],
+	endpoints: (builder) => ({}),
 });
