a77d80538fad6a87224e98ad5e92680c576d16d2
IXM138 <nyoman.gedar@gmail.com>
2022-11-13 16:39:52 +0000

    initialize post in postSlice and connect it to postSlice

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -8,51 +8,52 @@ const path = require("path");
 // @route GET /users
 // @access Private
 const getAllPost = asyncHandler(async (req, res) => {
-	console.log("Masuk get post");
-	// Get all posts from Database
-	const posts = await Post.find().lean();
+    console.log("Masuk get post");
+    // Get all posts from Database
+    const posts = await Post.find().lean();
 
-	if (!posts?.length) {
-		return res.status(400).json({ message: "No posts found" });
-	}
+    if (!posts?.length) {
+        return res.status(400).json({ message: "No posts found" });
+    }
 
-	res.json(posts);
+    res.json(posts);
 });
 
 const createNewPost = asyncHandler(async (req, res) => {
-	const { user, title, text } = req.body;
+    const { user, title, text } = req.body;
 
-	console.log("files + " + JSON.stringify(req.files));
+    console.log(JSON.stringify(req.body));
+    console.log("files + " + JSON.stringify(req.files));
 
-	if ((!user, !title, !text)) {
-		return res.status(400).json({ message: "All fields are required" });
-	}
+    if ((!user, !title, !text)) {
+        return res.status(400).json({ message: "All fields are required" });
+    }
 
-	const postObject = {
-		user,
-		title,
-		text,
-		image: req.files.image[0].filename,
-		image_wide: req.files.image_wide[0].filename,
-		image_mobile: req.files.image_mobile[0].filename,
-		// image: {
-		// 	data: fs.readFileSync(
-		// 		path.join(__dirname, "/../uploads/" + req.file.filename)
-		// 	),
-		// 	contentType: "image/png",
-		// },
-	};
+    const postObject = {
+        user,
+        title,
+        text,
+        image: req.files.image[0].filename,
+        image_wide: req.files.image_wide[0].filename,
+        image_mobile: req.files.image_mobile[0].filename,
+        // image: {
+        // 	data: fs.readFileSync(
+        // 		path.join(__dirname, "/../uploads/" + req.file.filename)
+        // 	),
+        // 	contentType: "image/png",
+        // },
+    };
 
-	const post = await Post.create(postObject);
+    const post = await Post.create(postObject);
 
-	if (post) {
-		res.status(201).json({ message: `New post created` });
-	} else {
-		res.status(400).json({ message: "Invalid post data" });
-	}
+    if (post) {
+        res.status(201).json({ message: `New post created` });
+    } else {
+        res.status(400).json({ message: "Invalid post data" });
+    }
 });
 
 module.exports = {
-	getAllPost,
-	createNewPost,
+    getAllPost,
+    createNewPost,
 };
--- frontend-app/src/components/Admin/AdminMenuPost.jsx
+++ frontend-app/src/components/Admin/AdminMenuPost.jsx
@@ -1,26 +1,130 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { useNewPostMutation } from "../../features/posts/postsApiSlice";
 
 const AdminMenuPost = () => {
+    const [addNewPost, { isLoading, isSuccess, isError, error }] =
+        useNewPostMutation();
+
+    const [user, setUser] = useState("635455214a4f413239a247e4");
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [link, setLink] = useState("");
+    const [active, setActive] = useState("");
+    const [image, setImage] = useState("");
+    const [imageW, setImageW] = useState("");
+    const [imageM, setImageM] = useState("");
+
+    useEffect(() => {
+        if (isSuccess) {
+            setTitle("");
+            setDescription("");
+            setLink("");
+            setActive("");
+            setImage(null);
+            setImageM(null);
+            setImageW(null);
+        }
+    }, [isSuccess]);
+
+    const onTitleChanged = (e) => setTitle(e.target.value);
+    const onDescriptionChanged = (e) => setDescription(e.target.value);
+    const onLinkChanged = (e) => setLink(e.target.value);
+    const onActiveChanged = (e) => setActive(e.target.value);
+    const onImageChanged = (e) => setImage(e.target.value);
+    const onImageWChanged = (e) => setImageW(e.target.value);
+    const onImageMChanged = (e) => setImageM(e.target.value);
+
+    const onSavePostClicked = async (e) => {
+        e.preventDefault();
+        console.log({ user, title, link, active });
+        await addNewPost(user, title, link, active, image, imageM, imageW);
+    };
+
+    const errClass = isError ? "errmsg" : "offscreen";
+
     return (
         <>
             <div>
                 <h1>Create a Post </h1>
-                <form className="row">
+                <form className="row" onSubmit={onSavePostClicked}>
+                    <div className="col-md-12">
+                        <label className="form-label">Title</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="title"
+                            name="title"
+                            value={title}
+                            onChange={onTitleChanged}
+                        />
+                    </div>
                     <div className="col-md-12">
                         <label className="form-label">Description</label>
-                        <textarea class="form-control" />
+                        <textarea
+                            class="form-control"
+                            id="description"
+                            name="desctiption"
+                            value={description}
+                            onChange={onDescriptionChanged}
+                        />
+                    </div>
+                    <div className="col-md-12">
+                        <label className="form-label">Link</label>
+                        <input
+                            class="form-control"
+                            type="text"
+                            id="link"
+                            name="link"
+                            value={link}
+                            onChange={onLinkChanged}
+                        />
+                    </div>
+                    <div className="col-md-12">
+                        <input
+                            type="checkbox"
+                            id="activate"
+                            name="activate"
+                            value={true}
+                            onChange={onActiveChanged}
+                        />
+                        <label className="form-check-label">Activate</label>
                     </div>
                     <div className="col-md-12">
                         <label className="form-label">Image large: </label>
-                        <input type="file" class="form-control-file" />
+                        <input
+                            type="file"
+                            class="form-control-file"
+                            id="image"
+                            name="image"
+                            value={image}
+                            onChange={onImageChanged}
+                        />
                     </div>
                     <div className="col-md-12">
-                        <label className="form-label">Image medium: </label>
-                        <input type="file" class="form-control-file" />
+                        <label className="form-label">Image wide: </label>
+                        <input
+                            type="file"
+                            class="form-control-file"
+                            id="imageW"
+                            name="imageW"
+                            value={imageW}
+                            onChange={onImageWChanged}
+                        />
                     </div>
                     <div className="col-md-12">
-                        <label className="form-label">Image small</label>
-                        <input type="file" class="form-control-file" />
+                        <label className="form-label">Image mobile:</label>
+                        <input
+                            type="file"
+                            class="form-control-file"
+                            id="imageM"
+                            name="imageM"
+                            value={imageM}
+                            onChange={onImageMChanged}
+                        />
+                    </div>
+                    <div className="col-md-12">
+                        <button type="submit">submit</button>
                     </div>
                 </form>
             </div>
