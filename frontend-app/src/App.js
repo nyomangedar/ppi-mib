3423381d4b940d659039cff0822205f2128ed3f0
adityojulian <axm1882@student.bham.ac.uk>
2022-11-18 20:11:23 +0000

    updated dependencies and removed error imports

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -5,7 +5,6 @@ import Layout from "./components/Layout";
 import Homepage from "./pages/Homepage";
 import "../node_modules/react-bootstrap/dist/react-bootstrap";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import HompageContent from "./components/Homepage/HompageContent";
 import PPIHeader from "./components/PPIHeader";
 import Footer from "./components/Footer";
 import "./index.css";
